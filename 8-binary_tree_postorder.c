f0917b48e6ef7fe67551afff9364623cb8ccdb1d
courte_labs <courte_labs@kali.courte_labs>
2022-08-24 17:57:09 +0300

    postorder traversal

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -7,7 +7,8 @@
  * @func: pointer to a function to call for each node
  * Return: void
  */
-void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));{
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
 	if (tree == NULL || func == NULL)
 		return;
 		
