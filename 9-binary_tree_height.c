073f86709484829bd7443a139c91f93737c2fc76
courte_labs <courte_labs@kali.courte_labs>
2022-08-24 18:11:13 +0300

    Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,17 +8,28 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t height_left = 0;
+	size_t height_right = 0;
+
 	if (tree->parent == NULL)
 	{
-		return;
+		return (0);
 	}
-	int count;
 
-	if (tree->parent->left != NULL)
+	height_left = binary_tree_height(tree->left);
+	height_right = binary_tree_height(tree->right);
+
+	if (tree->left || tree->right)
 	{
-		count++;
-	}
-	tree->parent->left = tree->parent;
-	return (count);
+		if (height_left > height_right)
+		{
+			return (height_left + 1);
+		}
+		else
+		{
+			return (height_right + 1);
+		}
 
+	}
+	return (0);
 }
