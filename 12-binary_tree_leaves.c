2e61db330e62da340894aa9624a25c429264ed6f
courte_labs <courte_labs@kali.courte_labs>
2022-08-24 23:48:36 +0300

    Leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,7 +8,11 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+
 	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
