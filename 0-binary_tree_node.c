b44d15a963fdd1f6fed14c2cd700bb4f66f5c30b
courte_labs <courte_labs@kali.courte_labs>
2022-08-24 13:36:55 +0300

    New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
     node = malloc(sizeof(binary_tree_t));
 
 
-    if (!node)
+    if (node == NULL)
     {
         return (NULL);
     }
