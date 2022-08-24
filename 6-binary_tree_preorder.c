caa281e41b8b3c3223df080bfa5553be665c064c
courte_labs <courte_labs@kali.courte_labs>
2022-08-24 17:48:51 +0300

    preorder tarversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -12,6 +12,11 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	{
 		return;
 	}
+
+	if (tree == NULL)
+	{
+		return;
+	}
 	
 	func(tree->n);
 	binary_tree_preorder(tree->left, func);
