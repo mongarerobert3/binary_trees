a81189dc3b673e558cdafec7effc5966244cc216
courte_labs <courte_labs@kali.courte_labs>
2022-08-24 15:55:22 +0300

    Insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,14 +19,16 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->parent = parent;
 	new_node->right = NULL;
 
-	if (value < parent)
+	if (parent->left != NULL)
 	{
-		new_node->left = value;
-		new_node->left = parent;
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
 
 	}
 	else
 	{
-		return (new_node);
+		new_node->left = new_node;
 	}
+	parent->left = new_node;
+	return (new_node);
 }
