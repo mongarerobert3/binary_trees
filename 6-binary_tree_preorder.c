823c602985151cbed606ca4b1bd6d34408a3b822
courte_labs <courte_labs@kali.courte_labs>
2022-08-24 17:50:28 +0300

    preorder tarversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -5,19 +5,12 @@
  *
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
+ * Return: void
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree->parent == NULL || func == NULL)
-	{
+	if (tree == NULL || func == NULL)
 		return;
-	}
-
-	if (tree == NULL)
-	{
-		return;
-	}
-	
 	func(tree->n);
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
