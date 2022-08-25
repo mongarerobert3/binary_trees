18aad87a609af5831c73f490ffbc82eca5cb5b65
courte_labs <courte_labs@kali.courte_labs>
2022-08-25 12:47:25 +0300

    is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_perfect - function that checks if a binary tree is full
+ * binary_tree_is_perfect - checks if a binary tree is full
  *
  * @tree: pointer to the root node of the tree to check
  * Return: int
@@ -9,7 +9,10 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    if (tree == NULL)
+	if (tree == NULL)
 		return (0);
-    return (binary_tree_is_perfect(tree->left) & binary_tree_is_perfect(tree->right));
+
+	if (tree->left || tree->right)
+		return (binary_tree_is_perfect(tree->left) & binary_tree_is_perfect(tree->right));
+	return (0);
 }
