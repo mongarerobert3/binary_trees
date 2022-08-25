80d8b3fa1a2c16efe37dbc5351f865b83a9e0395
courte_labs <courte_labs@kali.courte_labs>
2022-08-25 12:34:15 +0300

    Is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,8 +11,5 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (binary_tree_is_full(tree->left))
-		return (1);
-	if (binary_tree_is_full(tree->right))
-		return (1);
+	return (binary_tree_is_full(tree->left)  && binary_tree_is_full(tree->right));
 }
